e1bc98de0ed8fa0f7618ee8e8127aa67f2de3760
gnunez <gnunez@rdsistemas.com>
2023-04-13 11:51:31 +0200

    Calculadora de Rm

---
+++ app/src/main/java/com/example/learningstrenght/calculadoras/rm/CalculadoraRm.java
@@ -0,0 +1,36 @@
+package com.example.learningstrenght.calculadoras.rm;
+
+import android.text.Editable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculadoraRm {
+    private Integer peso;
+    private Integer repes;
+    private Map<String, Integer> mapRm;
+
+    public CalculadoraRm(Integer peso, Integer repes) {
+        this.peso = peso;
+        this.repes = repes;
+    }
+    public Map<String, Integer> getRm(){
+        mapRm = new HashMap<>();
+        mapRm.put("Brzycki", rmBrzycki());
+        mapRm.put("Epley", rmEpley());
+        return mapRm;
+    }
+
+    private Integer rmBrzycki() {
+        Integer arriba = peso * 36;
+        Integer abajo = 37 - repes;
+        if (abajo != 0)
+            return arriba / abajo;
+        else
+            return 0;
+    }
+
+    private Integer rmEpley() {
+        return (peso * (repes + 30)) / 30;
+    }
+}
