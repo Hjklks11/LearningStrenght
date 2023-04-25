8fa86111ba22607ae802a4e22a7028a7e3e29241
gnunez <gnunez@rdsistemas.com>
2023-04-25 09:26:31 +0200

    Perfil Usuario material

--- app/src/main/java/com/example/learningstrenght/PerfilUsuarioActivity.java
+++ app/src/main/java/com/example/learningstrenght/PerfilUsuarioActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -23,5 +25,18 @@ public class PerfilUsuarioActivity extends AppCompatActivity {
         user.getPhoneNumber();
         user.getPhotoUrl();
         user.getMetadata().getCreationTimestamp();
+    }
+
+    private void showDatePickerDialog() {
+        MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Fecha nacimiento").build();
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                //txtDate.setText(""+materialDatePicker.getHeaderText());
+            }
+        });
+
+        materialDatePicker.show(getSupportFragmentManager(), "TAG");
     }
 }
